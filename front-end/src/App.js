a27735da26e3fea6a51f34db977ada7ba63472c2
khmorad <khakho.morad@gmail.com>
2024-04-07 02:20:29 -0700

    implemented styling of navbar + file structureS

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,15 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-import Navbar from './Navbar';
-import Ladysuzan from './Ladysuzan';
-import Infowindow from './Infowindow';
-import Chatbot from './Chatbot';
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Ladysuzan from "./components/Ladysuzan";
+import Infowindow from "./components/Infowindow";
+import Chatbot from "./components/Chatbot";
+
 function App() {
   return (
     <div className="App">
-        <Navbar/>
-        <Ladysuzan/>
-        <Infowindow/>
+      <Navbar />
+      <Ladysuzan />
+      <Infowindow />
     </div>
   );
 }
---
+++ front-end/src/components/Ladysuzan.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import "react-image-gallery/styles/css/image-gallery.css"; // Import the CSS for styling
+import "../stylings/Ladysuzan.css";
+
+const images = [
+  {
+    original: "https://picsum.photos/id/1018/1000/600/",
+    thumbnail: "https://picsum.photos/id/1018/250/150/",
+  },
+  {
+    original: "https://picsum.photos/id/1015/1000/600/",
+    thumbnail: "https://picsum.photos/id/1015/250/150/",
+  },
+  {
+    original: "https://picsum.photos/id/1019/1000/600/",
+    thumbnail: "https://picsum.photos/id/1019/250/150/",
+  },
+];
+export default function Ladysuzan() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handleClickNext = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === images.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+
+  const handleClickPrev = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? images.length - 1 : prevIndex - 1
+    );
+  };
+
+  return (
+    <div>
+      <div className="gallery">
+        <button className="nav-button" onClick={handleClickPrev}>
+          {"<"}
+        </button>
+        <div className="gallery-item">
+          <img
+            src={images[currentIndex].original}
+            alt={`Image ${currentIndex}`}
+          />
+        </div>
+        <button className="nav-button" onClick={handleClickNext}>
+          {">"}
+        </button>
+      </div>
+    </div>
+  );
+}
