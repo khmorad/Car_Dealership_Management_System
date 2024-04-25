ff3e110c4ee8c7c4a715d12b739a623c99bd7a8c
khmorad <khakho.morad@gmail.com>
2024-04-24 18:51:15 -0700

    fix chat bugs 🐛

--- front-end/src/components/ChatAssistant.js
+++ front-end/src/components/ChatAssistant.js
@@ -39,7 +39,7 @@ export default function ChatAssistant() {
       const promptMessage = {
         role: "system",
         content:
-          "This is a chat for assisting Immigrants that have recently moved in to united states in they are in need of guidance",
+          "This is a chat for assisting Car dealership where It would answer anything car related",
       };
       const apiRequestBody = {
         model: "gpt-3.5-turbo",
