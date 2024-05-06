c83faf3533e8966205f16eaee0ea8dc56a68a6f7
khmorad <khakho.morad@gmail.com>
2024-05-05 17:54:03 -0700

    fix navbar logo

--- front-end/src/components/Navbar.js
+++ front-end/src/components/Navbar.js
@@ -5,22 +5,22 @@ import Login from "./Login";
 import Logo from "../assets/projicon.png"
 export default function Navbar() {
   const [showLogin, setShowLogin] = useState(false);
-  const [loginStatus, setLoginStatus] = useState(false); // Initialize login status as false
-  const [user, setUser] = useState(null); // State for current user
+  const [loginStatus, setLoginStatus] = useState(false); 
+  const [user, setUser] = useState(null); 
 
   const handleGoogleAuthClick = () => {
     setShowLogin(true);
   };
 
   const handleLogout = () => {
-    setLoginStatus(false); // Set login status to false
-    setUser(null); // Clear user state
-    setShowLogin(false); // Hide the login component
+    setLoginStatus(false); 
+    setUser(null); 
+    setShowLogin(false); 
   };
 
   return (
     <nav className="navbar">
-      <h1 className="logo"><img src={Logo} style={{height: "44px", width:"44px", }}/></h1>
+      <h1 className="logo" style={{display:"flex", alignItems: "center"}}><img src={Logo} style={{height: "44px", width:"44px", }}/> CDMS</h1>
       <ul className="nav-menu">
         <li className="nav-item">
           <a className="bruh" href="#home">
